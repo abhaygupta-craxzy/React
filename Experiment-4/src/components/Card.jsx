942d217b2e91b05afdd90a92829e6dadec36f93a
abhaygupta-craxzy <abhayloveporwal@gmail.com>
2026-04-24 00:15:50 +0530

    .\Experiment-4\

--- Experiment-4/src/components/Card.jsx
+++ Experiment-4/src/components/Card.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import "./Card.css"
+import React from "react";
+import "./Card.css";
 
 const Card = (props) => {
   return (
-
-    <div className='userCard'>
-        {/* <h1>Student Information</h1> */}
-        <div className='UserInfo'>
-            <h2 id='name'>{props.name}</h2>
-             <h3 id='course'>Course: {props.course}</h3>
-             <h3 id='marks'>Marks: {props.marks}</h3>
-             
-        </div>
+    <div className="userCard">
+      {/* <h1>Student Information</h1> */}
+      <div className="UserInfo">
+        <h2 id="name">{props.name}</h2>
+        <h3 className="course">
+          <span>Course:</span> {props.course}
+        </h3>
+        <h3 className="marks">
+          <span>Marks:</span> {props.marks}
+        </h3>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
